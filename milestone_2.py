d587b25ff11f4cc9982c1aaeea1ebf2bd0df8489
tonCodesData <124179394+tonCodesData@users.noreply.github.com>
2023-02-01 19:09:43 +0000

    completes task1

--- milestone_2.py
+++ milestone_2.py
@@ -1,1 +1,6 @@
-
+# Task 1
+# Step 1. Create a list containing the names of your 5 favorite fruits.
+# Step 2. Assign this list to a variable called word_list.
+word_list = ["banana", "apple", "grape", "orange", "mango"]
+# Step 3. Print out the newly created list to the standard output (screen).
+print(word_list)
