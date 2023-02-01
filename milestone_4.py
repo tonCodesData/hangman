a9aae31b4ade085ebdffcd778a7eda2d62588817
tonCodesData <124179394+tonCodesData@users.noreply.github.com>
2023-02-01 21:45:12 +0000

    does task 1

--- milestone_4.py
+++ milestone_4.py
@@ -1,1 +1,13 @@
+# task 1
+import random
+
+class Hangman:
+  def __init__(self, word_list, num_lives=5): 
+    self.word = random.choice(word_list)
+    self.word_guessed = ['_']*len(self.word)
+    self.num_letters = len(set(self.word))
+    self.num_lives = num_lives
+    self.word_list = word_list
+    self.list_of_guesses = []
+    
 
