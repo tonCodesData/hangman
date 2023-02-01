2a9add4f82e56c8edb42d42faf23888a6713e3ab
tonCodesData <124179394+tonCodesData@users.noreply.github.com>
2023-02-01 21:23:15 +0000

    completes task3

--- milestone_3.py
+++ milestone_3.py
@@ -1,15 +1,20 @@
-# task 1
-while True: 
-  guess = input("guess the letter ")
-  if len(guess) == 1 and guess.isalpha(): 
-    break
-  else: 
-    print("Invalid letter. Please, enter a single alphabetical character.")
-
-# task 2
 word = "apple"
 guess = "a"
-if guess in word: 
-  print(f"Good guess! {guess} is in the word.")
-else:
-  print(f"Sorry, {guess} is not in the word. Try again.")
+
+def check_guess(guess): 
+    guess = guess.lower()
+    if guess in word: 
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input(): 
+    while True: 
+        guess = input("guess the letter ")
+        if len(guess) == 1 and guess.isalpha(): 
+            break
+        else: 
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
