56c80bb8fba17d00683881372f99debb91be524a
tonCodesData <124179394+tonCodesData@users.noreply.github.com>
2023-02-01 23:06:21 +0000

    lets see

--- milestone_4.py
+++ milestone_4.py
@@ -1,34 +1,34 @@
-# task 1
 import random
 
 class Hangman:
-  def __init__(self, word_list, num_lives=5): 
-    self.word = random.choice(word_list)
-    self.word_guessed = ['_']*len(self.word)
-    self.num_letters = len(set(self.word))
-    self.num_lives = num_lives
-    self.word_list = word_list
-    self.list_of_guesses = []
+    def __init__(self, word_list, num_lives=5): 
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_']*len(self.word)
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
     
-  def check_guess(self, guess):
-    guess = guess.lower()
-
-    if guess in self.word: 
-      print(f"Good guess! {guess} is in the word.")
-
-  def ask_for_input(self):
-    while True:
-      self.guess = input("guess the letter ")
-      if not self.guess.isalpha() or len(self.guess)!=1 : 
-          print("Invalid letter. Please, enter a single alphabetical character.")
-          break
-      elif self.guess in self.list_of_guesses:
-          print("You already tried that letter!")
-          break
-      else: 
-          self.check_guess(self.guess)
-          break
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word: 
+            print(f"Good guess! {guess} is in the word.")
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+    
+    def ask_for_input(self):
+        while True:
+            self.guess = input("guess the letter ")
+            if not self.guess.isalpha() or len(self.guess)!=1 : 
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif self.guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else: 
+                self.check_guess(self.guess)
             
-new_game = Hangman(["apple", "banana"], 5)
-new_game.ask_for_input()
+            if self.num_lives == 0 or self.num_letters == 0:
+                break
+    
 
+Hangman(["apple", "banana"], 5).ask_for_input()
